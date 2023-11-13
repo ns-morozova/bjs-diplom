daacaf49e36d7347f8a08f45a072dc86ed88278d
Морозова Наталия <ns_morozova@mail.ru>
2023-11-13 21:17:39 +0300

    Первая часть работы

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,6 +1,26 @@
 "use strict";
+
+let call = function(f){
+    if (f.success) {
+        location.reload();
+    } else {
+        userForm.setLoginErrorMessage(f.error);
+    }
+}
+
 const userForm = new UserForm();
 userForm.loginFormCallback = function(data){
-    ApiConnector.login(data);
-    location.reload();
+    ApiConnector.login(data, call);
+}
+
+let registration = function(f){
+    if (f.success) {
+        location.reload();
+    } else {
+        userForm.setRegisterErrorMessage(f.error);
+    }
+}
+
+userForm.registerFormCallback = function(data){
+    ApiConnector.register(data, registration);
 }
